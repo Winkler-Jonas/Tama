69a3aa6c16b24790fe1210d3bf8993d8ac898b89
Jonas Winkler <jonas.winkler@hs-augsburg.de>
2024-05-21 22:44:10 +0200

    added pwa support to vite frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,3 +12,14 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// Register the service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('Service Worker registered: ', registration);
+    }).catch(registrationError => {
+      console.log('Service Worker registration failed: ', registrationError);
+    });
+  });
+}
