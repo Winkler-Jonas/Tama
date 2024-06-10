902e52ec5ca316206b365711d81a1e4f8d23c4da
Jonas Winkler <jonas.winkler@hs-augsburg.de>
2024-06-10 03:59:48 +0200

    finished ai backend

--- backend/tama_backend/ai/consumers.py
+++ backend/tama_backend/ai/consumers.py
@@ -1,19 +1,20 @@
-import os
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 import google.generativeai as genai
 from decouple import config
-from transformers import pipeline
+import regex as re
 
+response_pattern = re.compile(r"\*\*(?P<issue>.+?)\*\*", re.DOTALL | re.MULTILINE)
+
+# Configure the AI model
 genai.configure(api_key=config('DJANGO_GEMINI_AI_KEY'))
-nlp = pipeline("text-classification", model="unitary/toxic-bert")
 
 generation_config = {
     "temperature": 1,
     "top_p": 0.95,
     "top_k": 64,
-    "max_output_tokens": 8192,
+    "max_output_tokens": 100,
     "response_mime_type": "text/plain",
 }
 
@@ -26,6 +27,7 @@ model = genai.GenerativeModel(
 class AskAIConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.locale = self.scope['url_route']['kwargs'].get('locale', 'en')
+        await self.accept()
 
     async def disconnect(self, close_code):
         pass
@@ -33,62 +35,49 @@ class AskAIConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         try:
             text_data_json = json.loads(text_data)
-        except json.JSONDecodeError:
-            await self.send(text_data=json.dumps({'error': 'Invalid JSON format.'}))
-            return
-        try:
             user_question = text_data_json['question']
-            modified_question = self.modify_input(user_question)
+            modified_question = await self.modify_input(user_question)
             response = await self.ask_google_gemini(modified_question)
             final_response = self.process_response(response)
             await self.send(text_data=json.dumps({'message': final_response}))
-        except Exception as e:
+        except json.JSONDecodeError:
+            # frontend error
+            await self.send(text_data=json.dumps({'error': 'Invalid JSON format.'}))
+        except ValueError as e:
+            # user error / frontend error
             await self.send(text_data=json.dumps({'error': str(e)}))
+        except Exception as e:
+            # who knows
+            await self.send(text_data=json.dumps({'error': 'An unexpected error occurred.'}))
 
     @sync_to_async
     def modify_input(self, input_text):
-        result = nlp(input_text)
-        if any([r['label'] == 'LABEL_1' and r['score'] > 0.5 for r in result]):  # Check for offensive content
-            raise ValueError("Behave yourself!")
         if not input_text.strip():
             raise ValueError("Input text is empty.")
-        if len(input_text) > 300:
+        if len(input_text) > 100:
             raise ValueError("Input text is too long.")
 
         return (f"I want to do something and need 3 good ideas. "
-                f"One idea should not contain more than 10 words. "
-                f"Can you split the ideas with a '@' character. "
-                f"Also could you provide the answer in this language [{self.locale}]. "
+                f"One idea must not exceed 10 words. "
+                f"Answer in this language [{self.locale}]. "
                 f"This is the topic: [{input_text}]")
 
     def process_response(self, response):
-        topics = response.split('@')
-
-        if len(topics) >= 3:
-            formatted_response = {
-                'issue1': topics[0],
-                'issue2': topics[1],
-                'issue3': topics[2]
-            }
-        else:
-            formatted_response = {
-                'issue1': topics[0] if len(topics) > 0 else "Not provided",
-                'issue2': topics[1] if len(topics) > 1 else "Not provided",
-                'issue3': topics[2] if len(topics) > 2 else "Not provided"
-            }
+        formatted_response = {
+            'issue1': '',
+            'issue2': '',
+            'issue3': ''
+        }
+        if (match := response_pattern.findall(response)) and len(match) > 2:
+            formatted_response |= {key: match[idx] for idx, key in enumerate(formatted_response.keys())}
         return formatted_response
 
     @sync_to_async
     def ask_google_gemini(self, input_text):
         try:
-            # Start a chat session
             chat_session = model.start_chat(history=[])
-
-            # Send message to the model and get response
             response = chat_session.send_message(input_text)
+            print(response)
             return response.text
         except Exception as e:
-            # Handle specific exceptions or a general one
-            print(f"Error during API request: {str(e)}")
-            # Return a meaningful error message or use a fallback mechanism
-            return "We encountered an issue processing your request."
+            raise ValueError("Gemini-Error")
