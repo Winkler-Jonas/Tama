cee3c282226b197ddbbad533eea05c9ca7794c1a
Jonas Winkler <jonas.winkler@hs-augsburg.de>
2024-06-20 01:50:47 +0200

    added frontend api for feedback

---
+++ frontend/src/services/feedbackService.js
@@ -0,0 +1,22 @@
+import api from './api';
+
+class FeedbackService {
+    getAllFeedback() {
+        return api.get('/feedback/');
+    }
+
+    getFeedbackById(id) {
+        return api.get(`/feedback/${id}/`);
+    }
+
+    createFeedback(data) {
+        console.log(data)
+        return api.post('/feedback/', data);
+    }
+
+    deleteFeedback(id) {
+        return api.delete(`/feedback/${id}/`);
+    }
+}
+
+export default new FeedbackService();
